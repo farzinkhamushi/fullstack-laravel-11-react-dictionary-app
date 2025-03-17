2e4021e8ad19d1f3cba8082955fef8ce1656cfd7
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 10:25:03 +0330

    updating plan and adding some tables

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'number_of_hearts',
+        'price',
+        'price_id',
+    ];
+
+    public function subscriptions() 
+    {
+        return $this->hasMany(Subscription::class);
+    }
+
+    /*
+    
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function words()
+    {
+        return $this->hasMany(Word::class);
+    }
+
+    public function definitions()
+    {
+        return $this->hasMany(Definition::class);
+    }
+
+    public function synonyms()
+    {
+        return $this->hasMany(Synonym::class);
+    }
+    
+    public function antonyms()
+    {
+        return $this->hasMany(Antonym::class);
+    }
+
+    public function examples()
+    {
+        return $this->hasMany(Example::class);
+    }
+
+    */
 }
