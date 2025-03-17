511aa61df33e1e82028abee337f0a0f59225ace4
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 21:38:57 +0330

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,6 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -31,7 +30,6 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
