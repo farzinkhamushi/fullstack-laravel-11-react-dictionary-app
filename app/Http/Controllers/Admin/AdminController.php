64a03c0f76d421d579bac776f73ec741ce9ac684
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 13:22:39 +0330

    creating AuthAdminRequest and updating the AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,19 +1,59 @@
-<?php
+66<?php
 
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Definition;
+use App\Models\Word;
+use App\Models\Synonym;
+use App\Models\Plan;
+use App\Models\Subscription;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        $definition = Definition::all();
-        $synonym = Synonym::all();
-        $antonym = Antonym::all();
+        $definitions = Definition::all();
         $word = Word::all();
-        return view('admin.index', compact('definition', 'synonym', 'antonym', 'word'));
+        $synonyms = Synonym::all();
+        $plans = Plan::all();
+        $subscriptions = Subscription::all();
+
+        return view('admin.dashboard')->with([
+            'definitions' => $definitions,
+            'word' => $word,
+            'synonyms' => $synonyms,
+            'plans' => $plans,
+            'subscriptions' => $subscriptions,
+        ]);
     }
+
+    public function login(){
+        if(auth()->guard('admin')->check()){
+            return redirect()->route('admin.index');
+        }
+        return view('admin.login');
+    }
+    
+    public function auth(AuthAdminRequest $request){
+        if(auth()->guard('admin')->attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])){
+            $request->session()->regenerate();
+            return redirect()->route('admin.index');
+        }else{
+            throw ValidationException::withMessages([
+                'email' => 'These credentials do not match our records.',
+            ]);
+        }
+    }
+
+    public function logout(){
+        auth()->guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+
 
 }
