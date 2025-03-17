93c97c7c5f2341d2ce9d754df841d3411b00621a
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 12:02:35 +0330

    Update Word.php

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    protected $fillable = ['name', 'slug'];
+
+    public function definitions()
+    {
+        return $this->hasMany(Definition::class);
+    }
+    
+    public function synonyms()
+    {
+        return $this->hasMany(Synonym::class);
+    }
+    
 }
