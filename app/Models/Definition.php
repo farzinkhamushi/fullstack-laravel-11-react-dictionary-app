e812cf4ae805798cd911450ce4c110169abf0de8
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 10:06:47 +0330

    Update Definition.php

--- app/Models/Definition.php
+++ app/Models/Definition.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Definition extends Model
 {
-    //
+    protected $fillable = [
+        'word_id',
+        'meaning',
+        'part_of_speech',
+        'example_sentence',
+    ];
+
+    public function word()
+    {
+        return $this->belongsTo(Word::class);
+    }
+    
 }
