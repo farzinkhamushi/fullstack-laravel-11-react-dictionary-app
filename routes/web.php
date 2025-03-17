7697d954e1cf0212c55929f1a1b4a039ec35f780
Programming party <kuroshkhamushi@gmail.com>
2025-03-17 14:06:07 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,11 @@ use App\Http\Controllers\Admin\AdminController;
 
 
 Route::get('/', [AdminController::class,'login'])->name('admin.login');
-Route::post('admin/auth', [AdminController::class,'auth'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class,'auth'])->name('admin.auth');
+
+//Route::prefix('admin')->middleware('auth:admin')->group(function(){
+Route::prefix('admin')->middleware('admin')->group(function(){
+    Route::get('dashboard', [AdminController::class,'index'])->name('admin.index');
+    Route::post('logout', [AdminController::class,'logout'])->name('admin.logout');
+});
+
